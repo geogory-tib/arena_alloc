9cc53eeefb402fa7339024fa3124d243e0401fd8
geogory tibisov <geogorytibisov@gmail.com>
2025-11-06 14:30:36 -0600

    fixed buffer overflow in new page function

--- arena_alloc.h
+++ arena_alloc.h
@@ -13,7 +13,6 @@ typedef struct arena_t {
   int brk;
   int cap;
 } arena;
-static const int G_ALLOC_DEFAULT_AR_SIZE = (sizeof(arena)) * 10;
 static inline arena arena_new(int cap) {
   arena ret;
   ret.brk = 0;
@@ -64,16 +63,14 @@ typedef struct garena_t {
   int page_size;
   int current_page;
   int page_count;
-  arena alloc_ar; 
 } garena;
 
 static inline garena garena_new(int page_size) {
   garena ret;
-  ret.alloc_ar = arena_new(G_ALLOC_DEFAULT_AR_SIZE + 1);
   ret.page_size = page_size;
   ret.current_page = 0;
   ret.page_count = 10;
-  ret.pages = arena_alloc(&ret.alloc_ar, (sizeof(arena) * 10));
+  ret.pages = calloc(ret.page_count,sizeof(arena)); 
   ret.pages[ret.current_page] = arena_new(ret.page_size);
   return ret;
 }
@@ -82,11 +79,9 @@ static inline garena garena_new(int page_size) {
   you want to allocate your own pages.
 */
 static inline arena *garena_page_new(garena *ar) {
-  if (ar->current_page + 1 == ar->page_count) {
-    arena_realloc(&ar->alloc_ar,
-                  ar->alloc_ar.cap + G_ALLOC_DEFAULT_AR_SIZE + 1);
-    ar->pages = ar->alloc_ar.buffer;
-    ar->pages += 10;
+  if (ar->current_page + 1 >= ar->page_count) { 
+    ar->page_count += 10;
+    ar->pages = realloc(ar->pages, (sizeof(arena) * ar->page_count));
   }
   ar->pages[ar->current_page + 1] = arena_new(ar->page_size);
   ar->current_page++;
@@ -113,13 +108,13 @@ static inline int garena_used(garena *ar) {
 /*deconstructor for the garena*/
 static inline void garena_destroy(garena *gar) {
   for (int i = gar->current_page; i >= 0; i--) {
-    arena *current_page = &gar->pages[i];
-    arena_free(current_page);
+    //arena *current_page = &gar->pages[i];
+    arena_free( &gar->pages[i]);
   }
   gar->page_size = 0;
   gar->current_page = 0;
   gar->page_count = 0;
-  arena_free(&gar->alloc_ar);
+  free(gar->pages);
 }
 
 #endif // !
